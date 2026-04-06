0c3f8e35882be5f12ce40aa465f30a9d0efe67c1
nmonroe04 <167247190+nmonroe04@users.noreply.github.com>
2026-04-06 11:39:52 -0700

    Update pwm.h

--- ECE372Lab4/src/pwm.h
+++ ECE372Lab4/src/pwm.h
@@ -1,9 +1,7 @@
 #ifndef PWM_H
 #define PWM_H
 
-#include <avr/io.h>
-
-void initPWMTimer3();
-void changeDutyCycle(uint16_t adcValue);
+void initPWM3(void);
+void changeDutyCycle(unsigned int adcVal);
 
 #endif
